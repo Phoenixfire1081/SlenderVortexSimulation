f2b14f7f2a0f6ba8545d2ee80f107ef045a89baf
Abhishek Paraswarar Harikrishnan <abhishek02@zephyrus.imp.fu-berlin.de>
2023-07-05 13:29:23 +0200

    Added cleanup script to keep main directory clean.

---
+++ cleanup.py
@@ -0,0 +1,21 @@
+import os
+
+#---------------------------------------------------------------------#
+
+# ---------Analysis of Multiscale Data from the Geosciences---------- #
+
+# Author: Abhishek Harikrishnan
+# Email: abhishek.harikrishnan@fu-berlin.de
+# Last updated: 05-07-2023
+# Simple script to clean up main directory
+
+#---------------------------------------------------------------------#
+
+ignoreList = ['LICENSE', 'plotProfiles.py', 'README.md', 'Simulation_stagnant_LIA.py', \
+'Simulation_stagnant_M1KK.py', 'cleanup.py', 'Plots', 'Profiles', 'src', '.git',\
+'Simulation_stagnant_M1KK_PF.py', 'Simulation_stagnant_M1KK_FF.py']
+fileList = os.listdir('.')
+
+for i in fileList:
+	if not i in ignoreList:
+		os.system('rm -rf ' + i)
